5e756bcd838bcc80da574104a94a02fc5a35187f
levertyk <levertyk4517@my.uwstout.edu>
2022-12-01 10:59:14 -0600

    Initial Commit

---
+++ engine.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class engine{
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main() {
+        
+    }
+}
+
+// Write a program to implement an 8-bit Booth’s Multiplier for signed numbers.
+// •	Input in Decimal
+// •	Output in both Binary and Decimal
+// •	Show all the steps in a tabular format
+// In a word/pdf report, show test runs for the following input combinations:
+// 9 X 1, -9 X –9, 100 X 0, -9 X 9, 127 X -127, 500 X -5
